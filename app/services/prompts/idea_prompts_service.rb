cd21c302498c224b913d404e47144a8b13253d41
JigarShahTntra <jigar.shah@tntra.io>
2024-11-19 17:12:52 +0530

    market potential if it's availabel

--- app/services/prompts/idea_prompts_service.rb
+++ app/services/prompts/idea_prompts_service.rb
@@ -21,7 +21,7 @@ module Prompts
         Description
         "#{@idea.description}"
 
-        "#{"Market Potential Described by User \n" + @idea.market_potential if @idea.market_potential}"
+        "#{"Market Potential Described by User \n" + @idea.market_potential if @idea.market_potential.length > 5}"
 
         Transform this idea into a refined, professional business proposal that includes the following sections:
         1. Concept Overview: A clear and concise summary of the idea.
