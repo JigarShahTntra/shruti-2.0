1b0c64e6f75b8ae163d5f524e4ae70b64f00e689
JigarShahTntra <jigar.shah@tntra.io>
2024-11-21 16:19:19 +0530

    news and criteria with rating changes

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,6 +1,6 @@
 class NewsController < ActionController::API
     def index
-        newsapi = News.new(Rails.application.credentials.newsapi.key)
+        newsapi = News.new(Rails.application.credentials.newsapi.api_key)
         news = newsapi.get_everything(language: "en", q: "fintech", sortBy: "relevancy", searchIn: "title")
         render json: news.as_json, message: "News Fetched Successfully"
     end
