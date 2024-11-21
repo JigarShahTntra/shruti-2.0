97a1a2f92200e3e638df982c3703403b11a44de4
JigarShahTntra <jigar.shah@tntra.io>
2024-11-21 18:39:44 +0530

    pagesize added in news api

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,7 +1,7 @@
 class NewsController < ActionController::API
     def index
         newsapi = News.new(Rails.application.credentials.newsapi.api_key)
-        news = newsapi.get_everything(language: "en", q: "fintech", sortBy: "relevancy", searchIn: "title")
+        news = newsapi.get_everything(language: "en", q: "fintech", sortBy: "relevancy", searchIn: "title", pageSize: "14")
         render json: news.as_json, message: "News Fetched Successfully"
     end
 end
