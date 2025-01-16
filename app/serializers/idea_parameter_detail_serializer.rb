032927238e8bc68ff5412e04e2c807c55b4ca153
JigarShahTntra <jigar.shah@tntra.io>
2025-01-16 17:30:20 +0530

    added nil exception handling.

--- app/serializers/idea_parameter_detail_serializer.rb
+++ app/serializers/idea_parameter_detail_serializer.rb
@@ -4,7 +4,8 @@ class IdeaParameterDetailSerializer < ActiveModel::Serializer
   attribute :shruti_recommendations
 
   def shruti_recommendations
-    response = object.idea_parameter_recommendation_detail.idea_recommendation_formats.last
+    response = object.idea_parameter_recommendation_detail&.idea_recommendation_formats&.last
+    return nil unless response.present?
     r_response = {}
     r_response[object.stage_gate_parameter.cname] = { title: "#{object.stage_gate_parameter.name} Risk Factors and Mitigation Recommendations" }.merge(response.body)
   end
